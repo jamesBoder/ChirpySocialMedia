ecb715632ddfc162ee0f387d9987db9edf294566
jamesboder <james.macean08@gmail.com>
2025-09-08 19:44:08 -0400

    B: Create Server.

--- main.go
+++ main.go
@@ -16,6 +16,20 @@ func main() {
 		Handler: mux,
 	}
 
+	// use NewServeMux .Handle() to add a handler for the root path "/". Use .FileServer as the handler
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/app/", http.StatusMovedPermanently)
+	})
+
+	// Add a Readiness Endpoint
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
 	// Use the server ListenAndServe method to start the server
 	log.Println("Starting server on :8080")
 	if err := srv.ListenAndServe(); err != nil {
